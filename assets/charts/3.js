6ac49a8b61ff671c7c1d9658b7b8e65489ad9438
nap <nape43@gmail.com>
2026-08-31 14:50:13 +0900

    3.js を更新

--- assets/charts/3.js
+++ assets/charts/3.js
@@ -1,1 +1,17 @@
+window.songData_3 = {
+    id: 'song_3',
+    title: '星降る森の舞踏会',
+    artist: 'Silky Night',
+    jacket: '✨',
+    duration: 12.5,
+    chart: [
+        { time: 1.0, lane: 0, type: 'flick' },
+        { time: 1.0, lane: 4, type: 'flick' },
+        { time: 6.0, lane: 0, type: 'long', duration: 2.0 },
+        { time: 6.0, lane: 4, type: 'long', duration: 2.0 },
+        { time: 9.5, lane: 2, type: 'tap' },
+        { time: 11.0, lane: 1, type: 'flick' },
+        { time: 11.0, lane: 3, type: 'flick' }
+    ]
+};
 
