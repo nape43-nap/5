18ccef30b4fdec3a0b691d72912e2608d86cf5bf
nap <nape43@gmail.com>
2026-08-31 14:49:59 +0900

    2.js を更新

--- assets/charts/2.js
+++ assets/charts/2.js
@@ -1,1 +1,18 @@
+window.songData_2 = {
+    id: 'song_2',
+    title: 'お城のメヌエット',
+    artist: 'Royal Orchestra',
+    jacket: '🏰',
+    duration: 12.0,
+    chart: [
+        { time: 1.5, lane: 0, type: 'tap' },
+        { time: 3.0, lane: 4, type: 'flick' },
+        { time: 4.5, lane: 1, type: 'tap' },
+        { time: 4.5, lane: 3, type: 'tap' },
+        { time: 7.0, lane: 2, type: 'long', duration: 1.5 },
+        { time: 8.5, lane: 0, type: 'tap' },
+        { time: 8.5, lane: 4, type: 'flick' },
+        { time: 10.0, lane: 2, type: 'tap' }
+    ]
+};
 
