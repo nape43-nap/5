fd0e540a22bb536faff9dfa1e8f034e43fcafa85
nap <nape43@gmail.com>
2026-08-31 15:34:45 +0900

    sample.js を更新

--- assets/charts/sample.js
+++ assets/charts/sample.js
@@ -1,1 +1,9 @@
-
+window.songData_sample = {
+    id: 'sample',
+    title: 'sample',
+    artist: 'わし',
+    jacket: 'assets/images/sample.jpg',
+    audioSrc: 'assets/audio/sample.mp3',
+    duration: 7.1,
+    chart:     []
+};
