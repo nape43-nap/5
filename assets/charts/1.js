cb2e0f077f97cd384efdb3ffa471b115321e3caa
nap <nape43@gmail.com>
2026-08-31 14:49:37 +0900

    1.js を更新

--- assets/charts/1.js
+++ assets/charts/1.js
@@ -1,1 +1,27 @@
+window.songData_1 = {
+    id: 'song_1',
+    title: 'どんぐりころころ',
+    artist: '文部省唱歌',
+    jacket: '🌰', // 絵文字、または 'assets/images/1.png' などの画像パスでもOK！
+    duration: 16.0,
+    chart: [
+        { time: 1.0, noteName: 'G4', lane: 2, type: 'tap' },
+        { time: 1.5, noteName: 'E4', lane: 1, type: 'tap' },
+        { time: 2.0, noteName: 'G4', lane: 3, type: 'tap' },
+        { time: 2.5, noteName: 'E4', lane: 1, type: 'tap' },
+        { time: 3.0, noteName: 'C5', lane: 4, type: 'tap' },
+        { time: 3.5, noteName: 'A4', lane: 0, type: 'tap' },
+        { time: 4.0, noteName: 'G4', lane: 2, type: 'tap' },
+        { time: 5.5, noteName: 'C5', lane: 0, type: 'tap' },
+        { time: 5.5, noteName: 'C5', lane: 4, type: 'tap' },
+        { time: 6.2, noteName: 'A4', lane: 1, type: 'tap' },
+        { time: 6.8, noteName: 'G4', lane: 3, type: 'tap' },
+        { time: 7.5, noteName: 'E4', lane: 2, type: 'long', duration: 1.5 },
+        { time: 10.0, noteName: 'G4', lane: 0, type: 'flick' },
+        { time: 10.6, noteName: 'A4', lane: 1, type: 'tap' },
+        { time: 11.2, noteName: 'C5', lane: 2, type: 'tap' },
+        { time: 11.8, noteName: 'D4', lane: 3, type: 'tap' },
+        { time: 12.5, noteName: 'C5', lane: 4, type: 'flick' }
+    ]
+};
 
