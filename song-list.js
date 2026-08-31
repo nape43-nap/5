f9939100c5cdc0c8bf1d059233601c6aa0107368
nap <nape43@gmail.com>
2026-08-31 16:53:19 +0900

    song-list.js を更新

--- song-list.js
+++ song-list.js
@@ -1,7 +1,8 @@
+// 曲ファイルのパスやデータを管理するリスト
 window.allSongFiles = [
-    'assets/charts/1.js',
-    'assets/charts/2.js',
-    'assets/charts/3.js',
-    'assets/charts/sample.js'
+    { path: "assets/charts/1.js", dataKey: "songData_1" },
+    { path: "assets/charts/2.js", dataKey: "songData_2" },
+    { path: "assets/charts/3.js", dataKey: "songData_3" }
+    // 新しい曲を追加するときはここに増やすだけでOKです
+    // { path: "assets/charts/4.js", dataKey: "songData_4" },
 ];
-
