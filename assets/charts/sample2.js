3a226874907ad5dff251b8dee3b829c609881fdd
nap <nape43@gmail.com>
2026-08-31 17:33:19 +0900

    sample2.js を作成

---
+++ assets/charts/sample2.js
@@ -0,0 +1,114 @@
+window.songData_sample2 = {
+    id: 'sample2',
+    title: 'sample2',
+    artist: 'わし',
+    jacket: 'assets/images/sample2.jpg',
+    audioSrc: 'assets/audio/sample2.mp3',
+    duration: 14.2,
+    chart:     [
+        {
+            "time": 0,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "C3"
+        },
+        {
+            "time": 0.47,
+            "lane": 2,
+            "type": "tap",
+            "noteName": "E3"
+        },
+        {
+            "time": 0.94,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "C3"
+        },
+        {
+            "time": 1.88,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "C3"
+        },
+        {
+            "time": 2.34,
+            "lane": 4,
+            "type": "slide",
+            "noteName": "E4"
+        },
+        {
+            "time": 2.81,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "C3"
+        },
+        {
+            "time": 3.75,
+            "lane": 0,
+            "type": "long",
+            "duration": 1.88,
+            "noteName": "D3"
+        },
+        {
+            "time": 4.69,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "F3"
+        },
+        {
+            "time": 6.09,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "F3"
+        },
+        {
+            "time": 6.56,
+            "lane": 0,
+            "type": "tap",
+            "noteName": "G3"
+        },
+        {
+            "time": 7.03,
+            "lane": 2,
+            "type": "tap",
+            "noteName": "E3"
+        },
+        {
+            "time": 7.5,
+            "lane": 3,
+            "type": "long",
+            "duration": 1.61,
+            "noteName": "C3"
+        },
+        {
+            "time": 9.38,
+            "lane": 0,
+            "type": "slide",
+            "noteName": "F4"
+        },
+        {
+            "time": 10.31,
+            "lane": 2,
+            "type": "slide",
+            "noteName": "D4"
+        },
+        {
+            "time": 11.25,
+            "lane": 2,
+            "type": "flick",
+            "noteName": "C5"
+        },
+        {
+            "time": 13.13,
+            "lane": 2,
+            "type": "flick",
+            "noteName": "F5"
+        },
+        {
+            "time": 14.06,
+            "lane": 1,
+            "type": "flick",
+            "noteName": "E5"
+        }
+    ]
+};
