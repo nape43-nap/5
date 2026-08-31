0c42cab327d443b4a3e70e096b16268d899aa7a8
nap <nape43@gmail.com>
2026-08-31 17:33:52 +0900

    song-list.js を更新

--- song-list.js
+++ song-list.js
@@ -2,5 +2,6 @@ window.allSongFiles = [
     'assets/charts/1.js',
     'assets/charts/2.js',
     'assets/charts/3.js',
-    'assets/charts/sample.js'  // 好きな名前でOK！
+    'assets/charts/sample.js',
+    'assets/charts/sample2.js'  // 好きな名前でOK！
 ];
