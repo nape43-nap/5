8a87e89c1e272f39aff184d1fe79667dc30f6481
nap <nape43@gmail.com>
2026-08-31 19:26:03 +0900

    song-list.js の削除

--- song-list.js
+++
@@ -1,9 +0,0 @@
-window.allSongFiles = [
-    'assets/charts/1.js',
-    'assets/charts/2.js',
-    'assets/charts/3.js',
-    'assets/charts/sample.js',
-    'assets/charts/sample2.js',
-    'assets/charts/sample3.js',
-    // 好きな名前でOK！
-];
