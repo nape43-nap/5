a9ff8fb559b44984c17e998daac8996064a89f25
nap <nape43@gmail.com>
2026-08-31 19:26:16 +0900

    songlist.js を更新

--- songlist.js
+++ songlist.js
@@ -1,1 +1,7 @@
+// 曲の管理ファイル（追加・削除はここだけ！）
+const registeredSongFiles = [
+    "assets/charts/donguri.js",
+    "assets/charts/hoshi.js",
+    // "assets/charts/fuji.js" ← 消したいときはコメントアウトするか行を削除するだけ
+];
 
