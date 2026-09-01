d56ce8f5e8950b3f2f1979def4277962c38c3933
nap <nape43@gmail.com>
2026-09-01 14:53:48 +0900

    sam41.js を更新

--- assets/songs/sam41.js
+++ assets/songs/sam41.js
@@ -1,6 +1,144 @@
-window.songIds = [
-    "sam41",
-    "donguri"
-    // 新しい曲を追加するときは、ここにファイル名（ID）を1行追加してください
-];
-
+window.sam41 = {
+  "id": "sam41",
+  "title": "さんぷる",
+  "artist": "わし",
+  "jacket": "🎵",
+  "audioSrc": "assets/audio/4.mp3",
+  "duration": 15,
+  "chart": [
+    {
+      "time": 0,
+      "lane": 0,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "C3"
+    },
+    {
+      "time": 0.469,
+      "lane": 2,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "E3"
+    },
+    {
+      "time": 0.938,
+      "lane": 0,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "C3"
+    },
+    {
+      "time": 1.875,
+      "lane": 0,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "C3"
+    },
+    {
+      "time": 2.344,
+      "lane": 2,
+      "type": "flick",
+      "duration": 0,
+      "noteName": "E4"
+    },
+    {
+      "time": 2.813,
+      "lane": 0,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "C3"
+    },
+    {
+      "time": 3.75,
+      "lane": 1,
+      "type": "long",
+      "duration": 1.875,
+      "noteName": "D3"
+    },
+    {
+      "time": 4.688,
+      "lane": 3,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "F3"
+    },
+    {
+      "time": 6.094,
+      "lane": 3,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "F3"
+    },
+    {
+      "time": 6.563,
+      "lane": 4,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "G3"
+    },
+    {
+      "time": 7.031,
+      "lane": 2,
+      "type": "tap",
+      "duration": 0,
+      "noteName": "E3"
+    },
+    {
+      "time": 7.5,
+      "lane": 0,
+      "type": "long",
+      "duration": 1.611,
+      "noteName": "C3"
+    },
+    {
+      "time": 9.375,
+      "lane": 3,
+      "type": "flick",
+      "duration": 0,
+      "noteName": "F4"
+    },
+    {
+      "time": 10.313,
+      "lane": 1,
+      "type": "flick",
+      "duration": 0,
+      "noteName": "D4"
+    },
+    {
+      "time": 11.25,
+      "lane": 0,
+      "type": "slidenode",
+      "duration": 0,
+      "noteName": "C5",
+      "path": [
+        0,
+        1
+      ],
+      "direction": "right"
+    },
+    {
+      "time": 13.125,
+      "lane": 3,
+      "type": "slidenode",
+      "duration": 0,
+      "noteName": "F5",
+      "path": [
+        3,
+        4
+      ],
+      "direction": "right"
+    },
+    {
+      "time": 14.063,
+      "lane": 2,
+      "type": "slidenode",
+      "duration": 0,
+      "noteName": "E5",
+      "path": [
+        2,
+        3
+      ],
+      "direction": "right"
+    }
+  ]
+};
