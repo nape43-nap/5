b2475e6d58c4ff32ac1d14e0403c89b793865c96
nap <nape43@gmail.com>
2026-09-02 13:06:01 +0900

    Add files via upload

---
+++ songs/hoshinokiseki/chart.js
@@ -0,0 +1,409 @@
+window.songData_hoshinokiseki = {
+    "id": "hoshinokiseki",
+    "title": "星の軌跡",
+    "artist": "naptime(寝落ちP)",
+    "jacket": "songs/hoshinokiseki/hoshi.png",
+    "audioSrc": "songs/hoshinokiseki/audio.mp3",
+    "duration": 24,
+    "chart": [
+        {
+            "time": 1.63,
+            "lane": 0,
+            "type": "long",
+            "noteName": "C3",
+            "duration": 0.326
+        },
+        {
+            "time": 1.957,
+            "lane": 1,
+            "type": "long",
+            "noteName": "D3",
+            "duration": 0.326
+        },
+        {
+            "time": 2.283,
+            "lane": 2,
+            "type": "long",
+            "noteName": "E3",
+            "duration": 0.326
+        },
+        {
+            "time": 2.609,
+            "lane": 3,
+            "type": "long",
+            "noteName": "F3",
+            "duration": 0.978
+        },
+        {
+            "time": 3.587,
+            "lane": 4,
+            "type": "long",
+            "noteName": "G3",
+            "duration": 0.326
+        },
+        {
+            "time": 3.913,
+            "lane": 1,
+            "type": "long",
+            "noteName": "D3",
+            "duration": 0.326
+        },
+        {
+            "time": 4.565,
+            "lane": 2,
+            "type": "flick",
+            "noteName": "E4"
+        },
+        {
+            "time": 4.891,
+            "lane": 3,
+            "type": "flick",
+            "noteName": "F4"
+        },
+        {
+            "time": 5.217,
+            "lane": 4,
+            "type": "long",
+            "noteName": "G3",
+            "duration": 0.326
+        },
+        {
+            "time": 5.543,
+            "lane": 3,
+            "type": "long",
+            "noteName": "F3",
+            "duration": 0.326
+        },
+        {
+            "time": 5.87,
+            "lane": 1,
+            "type": "long",
+            "noteName": "D3",
+            "duration": 0.326
+        },
+        {
+            "time": 6.196,
+            "lane": 2,
+            "type": "tap",
+            "noteName": "E3"
+        },
+        {
+            "time": 6.359,
+            "lane": 4,
+            "type": "long",
+            "noteName": "G3",
+            "duration": 0.652
+        },
+        {
+            "time": 7.174,
+            "lane": 4,
+            "type": "flick",
+            "noteName": "G4"
+        },
+        {
+            "time": 7.5,
+            "lane": 0,
+            "type": "flick",
+            "noteName": "C4"
+        },
+        {
+            "time": 7.826,
+            "lane": 0,
+            "type": "long",
+            "noteName": "C3",
+            "duration": 0.652
+        },
+        {
+            "time": 7.826,
+            "lane": 4,
+            "type": "long",
+            "noteName": "G3",
+            "duration": 0.652
+        },
+        {
+            "time": 8.478,
+            "lane": 1,
+            "type": "long",
+            "noteName": "D3",
+            "duration": 0.652
+        },
+        {
+            "time": 8.478,
+            "lane": 3,
+            "type": "long",
+            "noteName": "F3",
+            "duration": 0.652
+        },
+        {
+            "time": 9.13,
+            "lane": 0,
+            "type": "tap",
+            "noteName": "C3"
+        },
+        {
+            "time": 9.13,
+            "lane": 4,
+            "type": "flick",
+            "noteName": "G4"
+        },
+        {
+            "time": 9.457,
+            "lane": 0,
+            "type": "flick",
+            "noteName": "C4"
+        },
+        {
+            "time": 9.457,
+            "lane": 4,
+            "type": "tap",
+            "noteName": "G3"
+        },
+        {
+            "time": 10.109,
+            "lane": 2,
+            "type": "tap",
+            "noteName": "E3"
+        },
+        {
+            "time": 10.435,
+            "lane": 0,
+            "type": "tap",
+            "noteName": "C3"
+        },
+        {
+            "time": 10.761,
+            "lane": 1,
+            "type": "tap",
+            "noteName": "D3"
+        },
+        {
+            "time": 11.087,
+            "lane": 2,
+            "type": "tap",
+            "noteName": "E3"
+        },
+        {
+            "time": 11.413,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "F3"
+        },
+        {
+            "time": 11.739,
+            "lane": 4,
+            "type": "tap",
+            "noteName": "G3"
+        },
+        {
+            "time": 12.065,
+            "lane": 1,
+            "type": "flick",
+            "noteName": "D4"
+        },
+        {
+            "time": 12.228,
+            "lane": 3,
+            "type": "flick",
+            "noteName": "F4"
+        },
+        {
+            "time": 13.043,
+            "lane": 4,
+            "type": "tap",
+            "noteName": "G3"
+        },
+        {
+            "time": 13.37,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "F3"
+        },
+        {
+            "time": 13.696,
+            "lane": 1,
+            "type": "tap",
+            "noteName": "D3"
+        },
+        {
+            "time": 14.022,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "F3"
+        },
+        {
+            "time": 14.348,
+            "lane": 4,
+            "type": "long",
+            "noteName": "G3",
+            "duration": 0.652
+        },
+        {
+            "time": 15,
+            "lane": 0,
+            "type": "long",
+            "noteName": "C3",
+            "duration": 0.652
+        },
+        {
+            "time": 15.652,
+            "lane": 2,
+            "type": "flick",
+            "noteName": "E4"
+        },
+        {
+            "time": 16.304,
+            "lane": 1,
+            "type": "tap",
+            "noteName": "D3"
+        },
+        {
+            "time": 16.63,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "F3"
+        },
+        {
+            "time": 16.956,
+            "lane": 4,
+            "type": "flick",
+            "noteName": "G4"
+        },
+        {
+            "time": 17.283,
+            "lane": 2,
+            "type": "tap",
+            "noteName": "E3"
+        },
+        {
+            "time": 17.446,
+            "lane": 0,
+            "type": "tap",
+            "noteName": "C3"
+        },
+        {
+            "time": 17.935,
+            "lane": 0,
+            "type": "tap",
+            "noteName": "C3"
+        },
+        {
+            "time": 18.261,
+            "lane": 0,
+            "type": "flick",
+            "noteName": "C4"
+        },
+        {
+            "time": 18.261,
+            "lane": 2,
+            "type": "long",
+            "noteName": "E3",
+            "duration": 1.304
+        },
+        {
+            "time": 18.587,
+            "lane": 4,
+            "type": "flick",
+            "noteName": "G4"
+        },
+        {
+            "time": 18.913,
+            "lane": 0,
+            "type": "flick",
+            "noteName": "C4"
+        },
+        {
+            "time": 19.239,
+            "lane": 4,
+            "type": "flick",
+            "noteName": "G4"
+        },
+        {
+            "time": 19.565,
+            "lane": 4,
+            "type": "tap",
+            "noteName": "G3"
+        },
+        {
+            "time": 19.891,
+            "lane": 3,
+            "type": "tap",
+            "noteName": "F3"
+        },
+        {
+            "time": 20.054,
+            "lane": 2,
+            "type": "tap",
+            "noteName": "E3"
+        },
+        {
+            "time": 20.543,
+            "lane": 2,
+            "type": "tap",
+            "noteName": "E3"
+        },
+        {
+            "time": 20.87,
+            "lane": 0,
+            "type": "slidenode",
+            "noteName": "C5",
+            "path": [
+                0,
+                1
+            ],
+            "direction": "right",
+            "startLane": 0,
+            "middleLane": 1,
+            "endLane": 1,
+            "curve": "arc",
+            "slideDuration": 0.5
+        },
+        {
+            "time": 21.522,
+            "lane": 2,
+            "type": "slidenode",
+            "noteName": "E5",
+            "path": [
+                2,
+                3
+            ],
+            "direction": "right",
+            "startLane": 2,
+            "middleLane": 3,
+            "endLane": 3,
+            "curve": "arc",
+            "slideDuration": 0.5
+        },
+        {
+            "time": 22.174,
+            "lane": 4,
+            "type": "slidenode",
+            "noteName": "G5",
+            "path": [
+                4,
+                3
+            ],
+            "direction": "left",
+            "startLane": 4,
+            "middleLane": 3,
+            "endLane": 3,
+            "curve": "arc",
+            "slideDuration": 0.5
+        },
+        {
+            "time": 22.826,
+            "lane": 2,
+            "type": "slidenode",
+            "noteName": "E5",
+            "path": [
+                2,
+                3
+            ],
+            "direction": "right",
+            "startLane": 2,
+            "middleLane": 3,
+            "endLane": 3,
+            "curve": "arc",
+            "slideDuration": 0.5
+        }
+    ]
+};
